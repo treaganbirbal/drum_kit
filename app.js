c1ef256828d246e5590cd7646eb97ceec4a01720
Treagan Birbal <treaganbirbal@pursuit.org>
2019-11-21 18:16:33 -0500

    query key data

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('keydown', (event) => {
-        const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const key = document.querySelector(`key[data-key="${event.keyCode}"]`)
+        if(!audio){
+            return;
+        }
+        audio.currentTime = 0;
+        audio.play()
     })
 })
