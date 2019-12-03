9e89b851d50db5fb22ae1a476f451d66fbb7b221
Treagan Birbal <treaganbirbal@pursuit.org>
2019-12-03 14:54:34 -0500

    bug-fix for class key

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('keydown', (event) => {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
-    const key = document.querySelector(`key[data-key="${event.keyCode}"]`)
-        if(!audio){
-            return;
-        }
-        audio.currentTime = 0;
-        audio.play()
-    })
+    window.addEventListener('keydown', playSound )
+
+    function removeTransition(event){
+        if(event.propertyName !== 'transform') return;
+        this.classList.remove('playing')
+    }
+    const keys = document.querySelectorAll('.key')
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 })
+
+function playSound(event) {
+        const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+        const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+            if(!audio){
+                return
+            }
+            audio.currentTime = 0;
+            audio.play()
+            key.classList.add("playing")
+}
